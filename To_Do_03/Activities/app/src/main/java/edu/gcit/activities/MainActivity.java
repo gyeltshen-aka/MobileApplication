4cd3a8306352d5d2c96d02cd17fe7517ecd238c1
Gyeltshen Wangchuk <12190052.gcit@rub.edu.bt>
2021-03-15 22:04:27 +0600

    Activities

---
+++ To_Do_03/Activities/app/src/main/java/edu/gcit/activities/MainActivity.java
@@ -0,0 +1,21 @@
+package edu.gcit.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void send(View view) {
+        Intent obj = new Intent(this,MainActivity2.class);
+        startActivity(obj);
+    }
+}
